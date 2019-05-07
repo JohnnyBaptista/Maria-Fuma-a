2950c833ad85042468d0d9d22a30afe4f8fa8cc1
Sara <sara.albuquerque1503@gmail.com>
2019-05-07 15:10:30 -0300

    Sobre 8.0
    Botão, fonte e localização modificados (Quase pronto)

--- src/assets/js/functions.js
+++ src/assets/js/functions.js
@@ -1,5 +1,13 @@
 <script>
-  AOS.init(
-      duration: 1200,
-  );
+<script>
+function myMap() {
+var mapProp= {
+  center:new google.maps.LatLng(51.508742,-0.120850),
+  zoom:5,
+};
+var map = new google.maps.Map(document.getElementById("googleMap"),mapProp);
+}
+</script>
+
+<script src="https://maps.googleapis.com/maps/api/js?key=YOUR_KEY&callback=myMaphttps://www.google.com.br/maps/place/Brasil/@-13.6562247,-69.7300916,4z/data=!3m1!4b1!4m5!3m4!1s0x9c59c7ebcc28cf:0x295a1506f2293e63!8m2!3d-14.235004!4d-51.92528?hl=pt-BR&authuser=0"></script>
 </script>
